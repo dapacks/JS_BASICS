d9e82bd02121c3fc8fdcf248d0dd3065e58079c3
Deepak <60600822+dapacks@users.noreply.github.com>
2023-11-03 18:44:28 +0530

    Add files via upload

---
+++ html/project-color-flipper/color_flip.js
@@ -0,0 +1,14 @@
+let myArray =[0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
+
+const btn= document.getElementById("btn")
+const color=document.querySelector('.color')
+
+btn.addEventListener("click",function()
+{   let hexcolor='#'
+    for(let i=0;i<6;i++)
+    {
+     hexcolor+=myArray[(Math.floor(Math.random()*myArray.length))]
+    }
+    color.textContent=hexcolor;
+    document.body.style.backgroundColor=hexcolor
+});
